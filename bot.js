5aec4ba0f7a4292dcc2b9bd1544a4b33b7409118
Spindlyskit <josh.lavelle1@gmail.com>
2017-10-25 17:34:59 +0100

    Add content

---
+++ bot.js
@@ -0,0 +1,72 @@
+const commando = require('discord.js-commando');
+const path = require('path');
+const oneLine = require('common-tags').oneLine;
+const sqlite = require('sqlite');
+const config = require('config.json')('./config.json');
+
+const modlog = require("./modlog.js");
+
+const client = new commando.Client({
+	owner: config.owner,
+	commandPrefix: '!'
+});
+
+client
+	.on('error', console.error)
+	.on('warn', console.warn)
+	.on('debug', console.log)
+	.on('ready', () => {
+		console.log(`Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
+	})
+	.on('disconnect', () => { console.warn('Disconnected!'); })
+	.on('reconnecting', () => { console.warn('Reconnecting...'); })
+	.on('commandError', (cmd, err) => {
+		if(err instanceof commando.FriendlyError) return;
+		console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+	})
+	.on('commandBlocked', (msg, reason) => {
+		console.log(oneLine`
+			Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
+			blocked; ${reason}
+		`);
+	})
+	.on('commandPrefixChange', (guild, prefix) => {
+		console.log(oneLine`
+			Prefix ${prefix === '' ? 'removed' : `changed to ${prefix || 'the default'}`}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	})
+	.on('commandStatusChange', (guild, command, enabled) => {
+		console.log(oneLine`
+			Command ${command.groupID}:${command.memberName}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	})
+	.on('groupStatusChange', (guild, group, enabled) => {
+		console.log(oneLine`
+			Group ${group.id}
+			${enabled ? 'enabled' : 'disabled'}
+			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
+		`);
+	});
+
+client.registry
+    .registerDefaults()
+    .registerGroups([
+		['info', 'Informational'],
+		['opt', 'Option'],
+		['driver', 'Driver'],
+		['mod', 'Moderator'],
+		['lead', 'Leader'],
+		['admin', 'Administrator'],
+		['rowner', 'Room Owner']
+    ])
+	.registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.setProvider(
+	sqlite.open(path.join(__dirname, 'database.sqlite3')).then(db => new commando.SQLiteProvider(db))
+).catch(console.error);
+
+    
+client.login(config.token);
