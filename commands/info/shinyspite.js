481b83e285f0c266aced45116aa5671535fd3edd
Spindlyskit <josh.lavelle1@gmail.com>
2017-11-26 19:34:41 +0000

    Add shiny sprites

---
+++ commands/info/shinyspite.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class ShinyspriteCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'shinysprite',
+            aliases: ['shinyimg'],
+            group: 'info',
+            memberName: 'shinysprite',
+            description: 'Get sprite for this pokemon.',
+            examples: ['data <pokemon/item/move/ability>'],
+            args: [
+                {
+                    key: 'arg1',
+                    prompt: 'What would you like the sprite for?',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    run(msg, { arg1 }) {
+        let parser = msg.client.parser;
+        let pkmn = parser.parsePokemon(arg1);
+
+        if (!pkmn) return msg.say(`${arg1} is not a pokemon!`);
+        else return msg.embed(new MessageEmbed()
+        .setImage(`http://play.pokemonshowdown.com/sprites/xyani-shiny/${pkmn.species.toLowerCase().replace(" ", "").replace('-', '')}.gif`))
+    }
+};
